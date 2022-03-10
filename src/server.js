ded3dad41fdfcbb0ee01960593833d53f8b43f68
Ranjan1501 <ranjankumar1757@gmail.com>
2022-03-10 22:13:19 +0530

    Added a Procfile.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const app = require("./index");
 const connect = require("./Config/db");
 require("dotenv").config();
+
 let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 
 app.listen(port, async () => {
   console.log(`Server listening on ${port}`);
